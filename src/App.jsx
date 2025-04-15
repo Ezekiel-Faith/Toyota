d56a00cd251708c048062d4c9c5544fc35104edf
Ezekie-Faith <ezekielfaith2000@gmail.com>
2025-04-15 13:20:40 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import AboutUs from './pages/AboutUs';
+import Footer from './pages/Footer';
+import Home from './pages/Home';
+import NavBar from './pages/NavBar';
+import NewsRoom from './pages/NewsRoom';
+import OurCompany from './pages/OurCompany';
+
+function App() {
+  return (
+    <>
+      <Home />
+      <NavBar />
+      <Footer />
+      <AboutUs />
+      <OurCompany />
+      <NewsRoom />
+    </>
+  );
+}
+
+export default App;
