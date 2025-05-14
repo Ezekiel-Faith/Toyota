df8a05dbf8056baf8e0c87f03c3d9c1a1fe83cea
Ezekie-Faith <ezekielfaith2000@gmail.com>
2025-05-14 13:13:33 +0100

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
-import { useState } from 'react'
-import './App.css'
-import OurCompany from './pages/OurCompany'
-import Footer from './components/Footer'
-
-
+import './App.css';
+import OurCompany from './pages/OurCompany';
+import Footer from './components/Footer';
 
 function App() {
-
- 
   return (
     <div>
-
-      
-     <OurCompany/>
-     <Footer/>
-      
-      
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
