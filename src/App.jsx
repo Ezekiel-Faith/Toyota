4cb78808664c31bb6623aa3dd375031c91798b60
Ezekie-Faith <ezekielfaith2000@gmail.com>
2025-05-14 13:39:00 +0100

    footer final fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import './App.css';
-<<<<<<< HEAD
 import Footer from './pages/Footer';
-import OurCompany from './pages/OurCompany';
-=======
-import OurCompany from './pages/OurCompany';
-import Footer from './components/Footer';
->>>>>>> df8a05dbf8056baf8e0c87f03c3d9c1a1fe83cea
+// import Footer from './components/Footer';
 
 function App() {
   return (
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 const Button = ({ display, onClick, className }) => {
-<<<<<<< HEAD
+// <<<<<<< HEAD
   return (
     <button className={className} onClick={onClick}>
       {display}
@@ -8,11 +8,4 @@ const Button = ({ display, onClick, className }) => {
 };
 
 export default Button;
-=======
-    return(
-        <button className={className} onClick={onClick}>{display}</button>
-    )
-}
 
-export default Button;
->>>>>>> df8a05dbf8056baf8e0c87f03c3d9c1a1fe83cea
