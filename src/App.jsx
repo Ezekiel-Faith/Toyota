6810b7d87d383433ab7aac0431ba0751c01c84d8
Ezekie-Faith <ezekielfaith2000@gmail.com>
2025-05-14 10:45:15 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import AboutUs from './pages/AboutUs';
 import Footer from './pages/Footer';
 import Home from './pages/Home';
-import NavBar from './pages/NavBar';
 import NewsRoom from './pages/NewsRoom';
 import OurCompany from './pages/OurCompany';
 
 function App() {
   return (
     <div>
-      <NavBar />
+      <AboutUs/>
     </div>
   );
 }
